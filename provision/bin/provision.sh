05951f3313e768bcd1cb62872f54a4b0caab8ebe
plxulin <32951898+plxulin@users.noreply.github.com>
2024-02-27 21:24:22 +0800

    Update provision.sh

--- provision/bin/provision.sh
+++ provision/bin/provision.sh
@@ -10,24 +10,24 @@ sudo dpkg -i /vagrant/provision/lib/${SPLUNK_DEB}
 # accessing the splunk version from the passed in deb file
 SPLUNK_VER=$(echo $SPLUNK_DEB | grep -oP '[0-9]\.[0-9]\.[0-9]' | cut -d '.' -f 1)
 
-# if the splunk version is 7 or greater
-# if [[ $SPLUNK_VER -ge 7 ]] ; then
-#
-#         # testing if the subversion of splunk is 7.1 or greater
-#         if [[ $( echo $SPLUNK_VER | cut -d '.' -f 2 ) -ge 1 ]] ; then
-#
-#                 # testing if the file does not exists for the username and password
-#                 if [[ ! -z  ${SPLUNK_HOME}/etc/system/local/user-seed.conf ]] ; then
-#
-#                         # setting password for splunk
-#                         sudo -u root bash -c "cat >> ${SPLUNK_HOME}/etc/system/local/user-seed.conf << EOF
-# [user_info]
-# USERNAME = admin
-# PASSWORD = ${SPLUNK_PASS}
-# EOF"
-#                 fi
-#         fi
-# fi
+if the splunk version is 7 or greater
+if [[ $SPLUNK_VER -ge 7 ]] ; then
+
+        # testing if the subversion of splunk is 7.1 or greater
+        if [[ $( echo $SPLUNK_VER | cut -d '.' -f 2 ) -ge 1 ]] ; then
+
+                # testing if the file does not exists for the username and password
+                if [[ ! -z  ${SPLUNK_HOME}/etc/system/local/user-seed.conf ]] ; then
+
+                        # setting password for splunk
+                        sudo -u root bash -c "cat >> ${SPLUNK_HOME}/etc/system/local/user-seed.conf << EOF
+[user_info]
+USERNAME = admin
+PASSWORD = ${SPLUNK_PASS}
+EOF"
+                fi
+        fi
+fi
 
 # Start Splunk
 sudo ${SPLUNK_BIN} start --accept-license
